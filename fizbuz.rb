696f3876f8a41588e91e17b74485da0fec2d6169
Tomohiro Nishimura <tomohiro68@gmail.com>
2012-05-12 20:52:56 +0900

    FC

--- fizbuz.rb
+++ fizbuz.rb
@@ -1,9 +1,9 @@
 # coding: utf-8
 
 1.upto(100) do |i|
-  if i % 15 == 0   then puts 'FizBuz'
-  elsif i % 5 == 0 then puts 'Buz'
-  elsif i % 3 == 0 then puts 'Fiz'
+  if    i % 15 == 0 then puts 'FizBuz'
+  elsif i %  5 == 0 then puts 'Buz'
+  elsif i %  3 == 0 then puts 'Fiz'
   else
     puts i
   end
